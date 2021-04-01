9049f49ff91c209099291fd7267fe313760ab4a8
Meena Siddharthan <67400125+MeenaSiddharthan@users.noreply.github.com>
2021-04-01 13:06:14 +0800

    Update LLLscraper.py

--- LLLscraper.py
+++ LLLscraper.py
@@ -4,9 +4,45 @@ Created on Tue Mar 30 13:06:33 2021
 
 @author: m.siddharthan
 """
+#Packages to scrape
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+
+##Packages to scrape infinite scrolling
+#import time
+#from selenium import webdriver
+#from urllib.parse import urljoin
+#
+##Code to get URL from inifinte scrolling
+#driver = webdriver.Chrome(executable_path=r"E:\Chromedriver\chromedriver_win32_chrome83\chromedriver.exe")
+#driver.get("https://shop.lululemon.com/c/sale/_/N-1z0xcuuZ8t6")
+#time.sleep(2)  # Allow 2 seconds for the web page to open
+#scroll_pause_time = 1 # You can set your own pause time. My laptop is a bit slow so I use 1 sec
+#screen_height = driver.execute_script("return window.screen.height;")   # get the screen height of the web
+#i = 1
+#
+#while True:
+#    # scroll one screen height each time
+#    driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))  
+#    i += 1
+#    time.sleep(scroll_pause_time)
+#    # update scroll height each time after scrolled, as the scroll height can change after we scrolled the page
+#    scroll_height = driver.execute_script("return document.body.scrollHeight;")  
+#    # Break the loop when the height we need to scroll to is larger than the total scroll height
+#    if (screen_height) * i > scroll_height:
+#        break 
+#
+###### Extract Reddit URLs #####
+#urls = []
+#baseurl = "https://shop.lululemon.com"
+#soup = BeautifulSoup(driver.page_source, "html.parser")
+#productlist = soup.find_all("div",{"class":"product-tile__details"})
+##Getting links
+#productlinks = []
+#for product in productlist:
+#    link = product.find("a",{"class":"link lll-font-weight-medium"}).get('href')
+#    productlinks.append(baseurl+link)
 
 baseurl = "https://shop.lululemon.com"
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36'}
